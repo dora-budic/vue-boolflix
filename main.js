4bff05a7422fd322361d9a23a747b98c57cd86fc
Dora Budić <dbudic0706@gmail.com>
2021-04-12 15:21:01 +0200

    Milestone 1 details

--- main.js
+++ main.js
@@ -4,15 +4,30 @@ var app = new Vue({
   el: '#root',
   data: {
     searchInput: '',
-    searchResults: []
+    searchResults: [],
+    noResults: false
   },
   methods: {
     getResults: function () {
       if (this.searchInput != '') {
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=76ba2aae3375d8d4f8e6654f74fad328&query=${this.searchInput}`)
           .then((response) => {
-            this.searchResults = response.data.results;
+            if (response.data.results.length != 0) {
+              this.searchResults = response.data.results;
+              this.searchInput = '';
+              this.noResults = false;
+            } else {
+              this.searchResults = [];
+              this.searchInput = '';
+              this.noResults = true;
+            }
           });
+      } else {
+        this.searchResults = [];
+        this.noResults = true;
+      }
+      if (this.searchResults.length == 0) {
+        this.noResults = true;
       }
     }
   },
