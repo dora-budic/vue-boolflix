3355399fed1a7844f125d1fe33f8b4d25cf60548
Dora Budić <dbudic0706@gmail.com>
2021-04-13 12:56:20 +0200

    Milestone 2

--- main.js
+++ main.js
@@ -4,17 +4,27 @@ var app = new Vue({
   el: '#root',
   data: {
     searchInput: '',
+    uri: 'https://api.themoviedb.org/3',
+    api_key: '76ba2aae3375d8d4f8e6654f74fad328',
+    lang: 'it',
+    movieResults: [],
+    tvResults: [],
     searchResults: [],
     noResults: false,
-    flags: ['de', 'en', 'es', 'fr', 'hr', 'it', 'pt', 'ru', 'zh'],
+
 },
   methods: {
     getResults: function () {
       if (this.searchInput != '') {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=76ba2aae3375d8d4f8e6654f74fad328&query=${this.searchInput}`)
-          .then((response) => {
-            if (response.data.results.length != 0) {
-              this.searchResults = response.data.results;
+        let movieRequest = `${this.uri}/search/movie?api_key=${this.api_key}&query=${this.searchInput}&language=${this.lang}`;
+        let tvRequest = `${this.uri}/search/tv?api_key=${this.api_key}&query=${this.searchInput}&language=${this.lang}`;
+        axios.all([axios.get(movieRequest), axios.get(tvRequest)])
+          .then(axios.spread((...responses) => {
+            this.movieResults = responses[0].data.results;
+            this.tvResults = responses[1].data.results;
+            let allResults = [...this.movieResults,...this.tvResults];
+            if (allResults.length != 0) {
+              this.searchResults = allResults;
               this.searchInput = '';
               this.noResults = false;
             } else {
@@ -22,13 +32,31 @@ var app = new Vue({
               this.searchInput = '';
               this.noResults = true;
             }
-          });
+          }));
       } else {
         this.searchResults = [];
         this.noResults = true;
       }
       if (this.searchResults.length == 0) {
         this.noResults = true;
+      }
+    },
+    hideImage: function (e) {
+      e.target.style.display = 'none';
+      e.target.nextElementSibling.style.display = 'block';
+    },
+    getTitle: function (object) {
+      if (object.title) {
+        return object.title;
+      } else {
+        return object.name;
+      }
+    },
+    getOriginalTitle: function (object) {
+      if (object.title) {
+        return object.original_title;
+      } else {
+        return object.original_name;
       }
     }
   },
